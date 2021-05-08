2c179eef61d930125eefd9d01184facdfa8e6315
Patrick Ng <itzsparty@gmail.com>
2021-05-08 13:43:25 -0400

    Add project files.

---
+++ LifePointCalculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YugiohApplication
+{
+    class LifePointCalculator
+    {
+        // basic params
+        public string Name;
+        public string DeckName;
+
+        // Initialize for both player life points
+        public int PlayerOneLifePts = 8000;
+        public int PlayerTwoLifePts = 8000;
+
+        // Base params for adding a certain amount
+        //of Life pts
+        public int AddThousand = 1000;
+        public int PayEight = 800;
+
+        //param for turn counter based
+        // on cards that rely on number of turns
+        public int TurnCounter = 0;
+
+        public void DisplayPlayerInfo()
+        {
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Deck: " + DeckName);
+        }
+        public void AddThousandLPs()
+        {
+            PlayerOneLifePts += AddThousand;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace YugiohApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LifePointCalculator player1 = new LifePointCalculator()
+            {
+                Name = "Pat",
+                DeckName = "Zefra"
+            };
+            LifePointCalculator player2 = new LifePointCalculator()
+            {
+                Name = "Brian",
+                DeckName = "ABC"
+            };
+
+            Console.WriteLine("Welcome to Duel Interface!");
+
+
+            while (true)
+            {
+                Console.WriteLine("Current Life Point State");
+
+                Console.WriteLine("|Player 1|");
+                player1.DisplayPlayerInfo();
+                Console.WriteLine("");
+                Console.WriteLine("|Player 2|");
+                player2.DisplayPlayerInfo();
+
+                Console.WriteLine("Choose your option.\n");
+                Console.WriteLine("1 to Add Life Points, 2 to Subtract Life Points, 3 to Add turn counter");
+                Console.WriteLine("Q to exit");
+
+                string userInput = Console.ReadLine();
+                if (int.TryParse(userInput, out int value))
+                {
+                    if (value == 1)
+                    {
+                        Console.WriteLine(Choose you option);
+                    }
+                    Console.WriteLine("test");
+                }
+                if (userInput.ToUpper() == "Q")
+                {
+                    Console.WriteLine("Exiting Program...");
+                    Console.ReadLine();
+                    return;
+                }
+            }
+           
+        }
+    }
+}
